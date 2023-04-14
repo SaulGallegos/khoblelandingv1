f87bcc936b6ab28d1e4df99bad48bd9739e58fe3
Saul Gallegos <saulladrondeguevara@gmail.com>
2023-04-13 21:01:58 -0600

    logo and title added/modified

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, Head, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta
+          name='description'
+          content='I hope this tutorial is helpful for you'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
