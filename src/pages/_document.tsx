bb42e679a69b77a2884a6b2f3278a3e737f32678
Saul Gallegos <saulladrondeguevara@gmail.com>
2023-05-04 16:56:55 -0600

    document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,11 @@ export default function Document() {
       <Head>
         <meta
           name='description'
-          content='I hope this tutorial is helpful for you'
+          content='We are building an AI-driven job platform that will change the way students find their ideal careers. 
+
+We focus on connecting students with not only well-compensated jobs, but also opportunities that align with their passions and interests, fostering long-term employee engagement and talent retention. Our Airbnb-inspired interface allows students to explore job postings while businesses search for potential talent. 
+
+By leveraging AI and machine learning, Khoble will streamline the job application process and offer personalized job matching, enabling students to apply instantly and efficiently. Khoble will also act as an all-in-one platform to help businesses manage their recruitment process.'
         />
       </Head>
       <body>
